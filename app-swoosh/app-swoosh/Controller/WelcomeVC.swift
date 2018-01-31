ed32ca777169dfc55b7f37844bf1bfdcfcb0e85b
renad <renadk@gmail.com>
2018-01-31 13:35:20 +0400

    Segues Programatically

---
+++ app-swoosh/app-swoosh/Controller/WelcomeVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  app-swoosh
+//
+//  Created by Renad El Ashy on 1/7/18.
+//  Copyright © 2018 devslopes. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeVC: UIViewController {
+
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+    
+    }
+    @IBAction func unwindFromSkillVC(unwindSegue: UIStoryboardSegue){
+    
+
+
+}
+}
+
